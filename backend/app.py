e099f8be3c2218b34cc2ed621e45e1a3b4d5b0fd
Mondem Jagadeesh <jagadeeshmondem@gmail.com>
2026-09-07 13:47:23 +0530

    modified app.py

--- backend/app.py
+++ backend/app.py
@@ -279,7 +279,4 @@ def status():
 # START FLASK SERVER
 # ==========================================
 if __name__ == "__main__":
-
-    app.run(
-        debug=True
-    )
+    app.run(host="0.0.0.0", port=5000, debug=True)
